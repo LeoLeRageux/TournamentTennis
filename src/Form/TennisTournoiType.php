764991eef004795b710bba3f65ad1445a5dea05a
Thomas Bouchet <Thomas.Bouchet@iutbayonne.univ-pau.fr>
2020-02-03 14:10:24 +0100

    Amélioration formulaire création tournoi

--- src/Form/TennisTournoiType.php
+++ src/Form/TennisTournoiType.php
@@ -7,6 +7,11 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+
 
 class TennisTournoiType extends AbstractType
 {
@@ -18,27 +23,68 @@ class TennisTournoiType extends AbstractType
             ->add('adresse')
             ->add('dateDebutTournoi')
             ->add('dateFinTournoi')
-            ->add('estVisible', null, [
+            ->add('estVisible', ChoiceType::class, array(
+                'choices'  => array(
+                    'Oui' => true,
+                    'Non' => false,),
                 'help' => 'Les non-inscrits au tournoi ne pourront pas voir les détails du tournoi (inscrits, gagnant, résultats des matchs ...).',
-            ])
-            ->add('surface')
-            ->add('categorieAge')
-            ->add('genreHomme')
-            ->add('description')
+                ))
+                
+            ->add('surface', ChoiceType::class, array(
+                'choices'  => array(
+                    'Dur' => null,
+                    'Terre Battue' => false,
+                    'Gazon' => true,
+                    'Indoor' => false,
+                )))
+            ->add('categorieAge', ChoiceType::class, array(
+                'choices'  => array(
+                    '11/12 ans' => null,
+                    '13/14 ans' => false,
+                    '15/16 ans' => true,
+                    '17/18 ans' => false,
+                    'Seniors' => null,
+                    '35+' => false,
+                    '40+' => true,
+                    '45+' => false,
+                    '50+' => false,
+                    '55+' => true,
+                    '60+' => false,
+                    '65+' => false,
+                    '70+' => true,
+                    '75+' => false,
+                )))
+            ->add('genreHomme',ChoiceType::class, array(
+                'choices'  => array(
+                    'Tournoi Homme' => true,
+                    'Tournoi Femme' => false,)))
+
+            ->add('description',TextareaType::class)
             ->add('dateDebutInscriptions')
             ->add('dateFinInscriptions')
-            ->add('inscriptionsManuelles', null, [
-                'help' => 'L inscription des joueurs sera faîte manuellement par le créateur du tournoi.',
-            ])
+            ->add('inscriptionsManuelles', ChoiceType::class, array(
+                'choices'  => array(
+                    'Oui' => true,
+                    'Non' => false,),
+                'help' => "L'inscription des joueurs sera faîte manuellement par le créateur du tournoi.",
+            ))
             ->add('nbPlaces')
-            ->add('motDePasse', null, [
+            ->add('motDePasse', PasswordType::class , [
                 'help' => 'Laisser vide si vous ne souhaitez pas de mot de passe.',
             ])
-            ->add('validationInscriptions', null, [
-                'help' => 'L organisateur pourra accepter/refuser les demandes d inscriptions' ,
-            ])
-            ->add('nbSetsGagnants')
-            ->add('statut')
+            
+
+            ->add('validationInscriptions', ChoiceType::class, array(
+                'choices'  => array(
+                    'Oui' => true,
+                    'Non' => false,),
+                'help' => "L'organisateur pourra accepter/refuser les demandes d'inscriptions" ,
+            ))
+            ->add('nbSetsGagnants', ChoiceType::class, array(
+                'choices'  => array(
+                    '2' => true,
+                    '3' => false,
+                )))
         ;
     }
 
