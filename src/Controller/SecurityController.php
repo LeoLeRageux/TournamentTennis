717709de75150b4b0c089854c9b83b0d8ca3b2ee
Thomas Bouchet <Thomas.Bouchet@iutbayonne.univ-pau.fr>
2020-03-23 14:24:55 +0100

    Création du formulaire d'inscription

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,6 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Entity\TennisUtilisateur;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Form\UserType;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SecurityController extends AbstractController
 {
@@ -32,5 +37,43 @@ class SecurityController extends AbstractController
     public function logout()
     {
         return $this->render('security/login.html.twig');
+    }
+
+
+    /**
+     * @Route("/inscription", name="app_inscription")
+     */
+    public function inscription(Request $request, UserPasswordEncoderInterface $encoder)
+    {
+        //Créer un utilisateur vide
+        $utilisateur = new TennisUtilisateur();
+       
+        //Création du formulaire permettant de saisir un utilisateur
+        $form = $this -> createForm(UserType::class,$utilisateur);
+        
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // Attribuer un rôle à l'utilisateur
+            //$utilisateur->setRoles(['ROLE_USER']);
+
+            // Encoder le mot de passe de l'utilisateur
+            //$encodagePassword = $encoder->encodePassword($utilisateur,$utilisateur->getPassword());
+           // $utilisateur->setPassword($encodagePassword);
+
+
+            //$entityManager = $this->getDoctrine()->getManager();
+            //Enregistrer l'utilisateur en base de données
+            //$entityManager->persist($utilisateur);
+            //$entityManager->flush();
+
+            return $this->redirectToRoute('app_login');
+       }
+
+       //Affiche la page présentant le formulaire d'inscription
+        return $this->render('security/creer_compte.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
